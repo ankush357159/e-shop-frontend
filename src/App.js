f2001304692827cc50e0898ff881ad68171853aa
Ankush <ankush357159@gmail.com>
2021-11-21 00:47:58 +0530

    Loading and updating user

--- src/App.js
+++ src/App.js
@@ -9,8 +9,12 @@ import ProductDetails from "./component/Product/ProductDetails";
 import Products from "./component/Product/Products";
 import Search from "./component/Product/Search";
 import LoginSignUp from "./component/User/LoginSignUp";
+import UserOptions from "./component/layout/Header/UserOptions";
+import { useSelector } from "react-redux";
+import Profile from "./component/User/Profile";
 
 function App() {
+  const { isAuthenticated, user } = useSelector((state) => state.user);
   useEffect(() => {
     WebFont.load({
       google: {
@@ -21,11 +25,13 @@ function App() {
   return (
     <Router>
       <Header />
+      {isAuthenticated && <UserOptions user={user} />}
       <Route exact path='/' component={Home} />
       <Route exact path='/product/:id' component={ProductDetails} />
       <Route exact path='/products' component={Products} />
       <Route path='/products/:keyword' component={Products} />
       <Route path='/search' component={Search} />
+      <Route path='/account' component={Profile} />
       <Route exact path='/login' component={LoginSignUp} />
       <Footer />
     </Router>
