b295c5aaf250811b267504b3f3d883e25df242d5
Ankush <ankush357159@gmail.com>
2021-11-22 02:05:39 +0530

    Cart

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import UpdateProfile from "./component/User/UpdateProfile";
 import UpdatePassword from "./component/User/UpdatePassword";
 import ForgotPassword from "./component/User/ForgotPassword";
 import ResetPassword from "./component/User/ResetPassword";
-import Cart from "./component/Cart/Cart.js";
+import Cart from "./component/Cart/Cart";
 
 function App() {
   const { isAuthenticated, user } = useSelector((state) => state.user);
